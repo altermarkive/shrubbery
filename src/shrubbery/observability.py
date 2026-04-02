8dd1ed4a24afd9e41f0b56c75a8848b389f46b65
marek-burza <marek-burza@users.noreply.github.com>
2026-04-02 13:18:48 +0200

    Make distributed less chatty

--- src/shrubbery/observability.py
+++ src/shrubbery/observability.py
@@ -7,7 +7,7 @@ import logging
 def setup_logger() -> logging.Logger:
     logging_format = '%(asctime)s %(levelname)s: %(message)s'
     logging.basicConfig(format=logging_format, level=logging.INFO)
-    logging.getLogger('distributed.core').setLevel(logging.WARNING)
+    logging.getLogger('distributed').setLevel(logging.WARNING)
     logger = logging.getLogger('shrubbery')
     return logger
 
