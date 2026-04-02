8fa937b337341a1c8194e81dcc765bb2c9becadd
altermarkive <altermarkive@gmail.com>
2026-04-02 13:18:49 +0200

    Removed unised batched_fit from Pairwise

--- src/shrubbery/pairwise.py
+++ src/shrubbery/pairwise.py
@@ -15,53 +15,34 @@ class Pairwise(BaseEstimator, MetaEstimatorMixin, RegressorMixin):
         estimator: Any,
         n_shuffles_fit: int,
         n_shuffles_predict: int,
-        batched_fit: bool,
     ) -> None:
         self.estimator = estimator
         self.n_shuffles_fit = n_shuffles_fit
         self.n_shuffles_predict = n_shuffles_predict
-        self.batched_fit = batched_fit
 
     def fit(
         self, x: np.ndarray, y: np.ndarray, **kwargs: dict[str, Any]
     ) -> 'Pairwise':
         y = y.reshape(-1, 1)
-        if self.batched_fit:
-            for _ in range(self.n_shuffles_fit):
-                logger.info('Fitting...')
-                shuffled_x, shuffled_y = shuffle(x, y)
-                joint_x = np.concatenate([x, shuffled_x], axis=1)
-                selected = (y != shuffled_y).ravel()
-                joint_x = joint_x[selected]
-                joint_y = y[selected].ravel()
-                start_time = time.time()
-                self.estimator = self.estimator.fit(joint_x, joint_y)
-                stop_time = time.time()
-                logger.info(f'Fitted in {int(stop_time - start_time)}s')
-                mse = mean_squared_error(
-                    joint_y, self.estimator.predict(joint_x)
-                )
-                logger.info(f'MSE: {mse}')
-        else:
-            logger.info('Fitting...')
-            joint_x_list = []
-            joint_y_list = []
-            for _ in range(self.n_shuffles_fit):
-                shuffled_x, shuffled_y = shuffle(x, y)
-                joint_x = np.concatenate([x, shuffled_x], axis=1)
-                selected = (y != shuffled_y).ravel()
-                joint_x = joint_x[selected]
-                joint_y = y[selected]
-                joint_x_list.append(joint_x)
-                joint_y_list.append(joint_y)
-            joint_x = np.concatenate(joint_x_list)
-            joint_y = np.concatenate(joint_y_list).ravel()
-            start_time = time.time()
-            self.estimator = self.estimator.fit(joint_x, joint_y)
-            stop_time = time.time()
-            logger.info(f'Fitted in {int(stop_time - start_time)}s')
-            mse = mean_squared_error(joint_y, self.estimator.predict(joint_x))
-            logger.info(f'MSE: {mse}')
+        logger.info('Fitting...')
+        joint_x_list = []
+        joint_y_list = []
+        for _ in range(self.n_shuffles_fit):
+            shuffled_x, shuffled_y = shuffle(x, y)
+            joint_x = np.concatenate([x, shuffled_x], axis=1)
+            selected = (y != shuffled_y).ravel()
+            joint_x = joint_x[selected]
+            joint_y = y[selected]
+            joint_x_list.append(joint_x)
+            joint_y_list.append(joint_y)
+        joint_x = np.concatenate(joint_x_list)
+        joint_y = np.concatenate(joint_y_list).ravel()
+        start_time = time.time()
+        self.estimator = self.estimator.fit(joint_x, joint_y)
+        stop_time = time.time()
+        logger.info(f'Fitted in {int(stop_time - start_time)}s')
+        mse = mean_squared_error(joint_y, self.estimator.predict(joint_x))
+        logger.info(f'MSE: {mse}')
         self.fitted_ = True
         return self
 
