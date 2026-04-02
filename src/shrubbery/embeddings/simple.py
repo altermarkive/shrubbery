196d14e78a65fcaf5106afeb2dcf1e027a16c4d4
altermarkive <altermarkive@gmail.com>
2026-04-02 13:18:48 +0200

    Generalized PersistableEmbedder

--- src/shrubbery/embeddings/simple.py
+++ src/shrubbery/embeddings/simple.py
@@ -110,3 +110,19 @@ class GenericEmbedder(BaseEstimator, TransformerMixin):
                 )
             )
         return np.concatenate([eras] + [features] + embeddings + [types], axis=1)
+
+
+class PersistableEmbedder(BaseEstimator, TransformerMixin):
+    def __init__(
+        self,
+        estimator: TransformerMixin,
+    ) -> None:
+        self.estimator = estimator
+
+    def fit(self, x: NDArray, y: NDArray) -> 'PersistableEmbedder':
+        self.embedder_ = self.estimator.fit(x)
+        return self
+
+    def transform(self, x: NDArray) -> NDArray:
+        assert self.embedder_ is not None
+        return self.embedder_.transform(x)
