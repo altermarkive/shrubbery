d93455a86a859ef05f0f76f03686dde61ac06f39
marek-burza <marek-burza@users.noreply.github.com>
2026-04-02 13:18:48 +0200

    Let ruff show it would reformat

--- run.py
+++ run.py
@@ -84,7 +84,7 @@ def run_docker(arguments: argparse.Namespace) -> None:
         command.extend(
             [
                 '-c',
-                '"ruff check --select I src && ruff format --check && mypy src"',  # noqa: E501
+                '"ruff check --select I src && ruff format --check --diff && mypy src"',  # noqa: E501
             ]
         )
     if not arguments.lint and not arguments.debug:
