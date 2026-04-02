4404b3475bc034e53081861658d7b8b922ab79c4
altermarkive <altermarkive@gmail.com>
2026-04-02 13:18:49 +0200

    Removed unused PersistableEmbedder

--- src/shrubbery/embeddings/simple.py
+++ src/shrubbery/embeddings/simple.py
@@ -74,19 +74,3 @@ class GenericEmbedder(BaseEstimator, TransformerMixin):
                 )
             )
         return np.concatenate([eras] + [features] + embeddings, axis=1)
-
-
-class PersistableEmbedder(BaseEstimator, TransformerMixin):
-    def __init__(
-        self,
-        estimator: TransformerMixin,
-    ) -> None:
-        self.estimator = estimator
-
-    def fit(self, x: np.ndarray, y: np.ndarray) -> 'PersistableEmbedder':
-        self.embedder_ = self.estimator.fit(x)
-        return self
-
-    def transform(self, x: np.ndarray) -> np.ndarray:
-        assert self.embedder_ is not None
-        return self.embedder_.transform(x)
